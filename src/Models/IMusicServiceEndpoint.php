8514f6abe52547d2e119e878aa44f43a587f05bd
Oguzhan Uysal <development@oguzhanuysal.eu>
2016-05-19 11:56:35 +0200

    updated docblocks

--- src/Models/IMusicServiceEndpoint.php
+++ src/Models/IMusicServiceEndpoint.php
@@ -4,17 +4,44 @@ namespace Pbxg33k\MusicInfo\Models;
 
 interface IMusicServiceEndpoint
 {
+    /**
+     * IMusicServiceEndpoint constructor.
+     * @param $apiClient
+     */
     public function __construct($apiClient);
 
+    /**
+     * @return mixed
+     */
     function getApiService();
 
+    /**
+     * @param $arguments
+     * @return mixed
+     */
     public function get($arguments);
 
+    /**
+     * @param $arguments
+     * @return mixed
+     */
     public function getComplete($arguments);
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function getById($id);
 
+    /**
+     * @param $name
+     * @return mixed
+     */
     public function getByName($name);
 
+    /**
+     * @param $guid
+     * @return mixed
+     */
     public function getByGuid($guid);
 }
