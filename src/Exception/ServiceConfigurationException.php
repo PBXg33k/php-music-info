a47829fe6cd75de504fbe07726d8b5e19680c130
Oguzhan Uysal <development@oguzhanuysal.eu>
2016-05-22 20:34:55 +0200

    Replaced codeception with PHPUnit

---
+++ src/Exception/ServiceConfigurationException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PBX_g33k
+ * Date: 22-May-16
+ * Time: 17:29
+ */
+
+namespace Exception;
+
+
+class ServiceConfigurationException
+{
+
+}
---
+++ src/Exception/ServiceException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PBX_g33k
+ * Date: 22-May-16
+ * Time: 17:30
+ */
+
+namespace Exception;
+
+
+class ServiceException
+{
+
+}
