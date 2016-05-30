002cb0b02c20e84d792b5b262f668dda4df8033e
Oguzhan Uysal <development@oguzhanuysal.eu>
2016-05-30 13:33:50 +0200

    Composer updated and first work on Spotify support

---
+++ src/Service/Spotify/Service.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PBX_g33k
+ * Date: 30-May-16
+ * Time: 13:01
+ */
+
+namespace Pbxg33k\MusicInfo\Service\Spotify;
+
+use Pbxg33k\MusicInfo\Service\BaseService;
+use SpotifyWebAPI\Session;
+use SpotifyWebAPI\SpotifyWebAPI;
+use Symfony\Component\HttpFoundation\Request;
+
+class Service extends BaseService
+{
+    /**
+     * Is the client authorized via OAuth2.0
+     *
+     * @var bool
+     */
+    protected $authorized = false;
+
+    /**
+     * @var Session
+     */
+    protected $spotifySession;
+
+    /**
+     * @param array $config
+     */
+    public function init($config = [])
+    {
+        if(!$config) {
+            $config = $this->getConfig()['spotify'];
+        }
+
+        $this->spotifySession = new Session($config['client_id'], $config['client_secret'], $config['redirect_uri']);
+        $this->setApiClient(new SpotifyWebAPI());
+    }
+
+    /**
+     * @param $scopes
+     *
+     * @return mixed
+     */
+    public function requestCredentialsToken($scopes)
+    {
+        $this->spotifySession->requestCredentialsToken($scopes);
+        return $this->setAccessTokenFromSession();
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return string
+     */
+    public function requestAccessToken(Request $request)
+    {
+        $this->spotifySession->requestAccessToken($request->query->get('code'));
+        $this->setAccessTokenFromSession();
+        return $this->getApiClient()->getAccessToken();
+    }
+
+    /**
+     * @return bool
+     */
+    public function refreshTokens()
+    {
+        return $this->spotifySession->refreshAccessToken($this->spotifySession->getRefreshToken());
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function setAccessTokenFromSession()
+    {
+        return $this->getApiClient()->setAccessToken($this->spotifySession->getAccessToken());
+    }
+}
