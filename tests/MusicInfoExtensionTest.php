b01c4f88023198e7f7ced84eb4603559cc74270c
Oguzhan Uysal <development@oguzhanuysal.eu>
2016-10-11 22:56:41 +0200

    Add test for better code coverage

---
+++ tests/MusicInfoExtensionTest.php
@@ -0,0 +1,11 @@
+<?php
+use Pbxg33k\MusicInfo\DependencyInjection\MusicInfoExtension;
+
+class MusicInfoExtensionTest extends PHPUnit_Framework_TestCase
+{
+    public function testExtension()
+    {
+        $ext = new MusicInfoExtension();
+        $ext->load([], new \Symfony\Component\DependencyInjection\ContainerBuilder());
+    }
+}
