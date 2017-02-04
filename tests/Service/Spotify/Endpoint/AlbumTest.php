60a12b61ab7b5fcfaf61c0ee231c51ce01a25ac6
Oguzhan Uysal <development@oguzhanuysal.eu>
2017-02-04 12:36:11 +0100

    Update Copyright

--- tests/Service/Spotify/Endpoint/AlbumTest.php
+++ tests/Service/Spotify/Endpoint/AlbumTest.php
@@ -1,4 +1,13 @@
 <?php
+/*******************************************************************************
+ * This file is part of the Pbxg33k\MusicInfo package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * (c) 2017 Oguzhan uysal. All rights reserved
+ ******************************************************************************/
+
 namespace Service\Spotify\Endpoint;
 
 use Doctrine\Common\Collections\ArrayCollection;
